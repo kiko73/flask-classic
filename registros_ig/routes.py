e4528e30a98c20e30e9676a9d57b2337a2246162
kiko73 <kikovalledelrio@gmail.com>
2024-01-11 18:41:02 +0100

    segunda

--- registros_ig/routes.py
+++ registros_ig/routes.py
@@ -1,5 +1,10 @@
 from registros_ig import app
+from flask import render_template
 
 @app.route("/")
-def hello():
-    return "Hola esto es flask classic"
+def index():
+    diccionario = [{"fecha":"2024-01-01","concept":"Nomina enero","monto":1500},
+                   {"fecha":"2024-01-02","concept":"Compra de reyes","monto":-100},
+                   {"fecha":"2024-01-02","concept":"Compra de mercadona","monto":-100},
+                   {"fecha":"2024-01-02","concept":"Compra de roscón","monto": -20}]
+    return render_template("index.html",variable= diccionario)
